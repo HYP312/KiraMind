e296e7a2a8a9c60dbf681452e4ba91fe222452a6
Omega Yorha <65727048+HYP312@users.noreply.github.com>
2025-07-07 00:00:44 +0700

    Update styles.ts

--- component/styles.ts
+++ component/styles.ts
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  // Add your Neumorphic styles, spacing, typography, and color variables here
+  // additional style
 });
